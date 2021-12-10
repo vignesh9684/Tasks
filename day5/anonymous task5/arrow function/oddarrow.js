f9a90c47f3cbfb20603a9a69d4e2ff96abe9d74f
vignesh9684 <75724697+vignesh9684@users.noreply.github.com>
2021-12-11 01:30:50 +0530

    Delete day5/anonymous task5 directory

--- day5/anonymous task5/arrow function/oddarrow.js
+++
@@ -1,7 +0,0 @@
-let numberArray = [1, 2, 3, 4, 5, 6, 98, 101, 467]
-
-console.log("Odd Numbers in Number Array are: " + numberArray.filter((item) => {
-
-    return item % 2 !== 0
-
-}))
--- day5/anonymous task5/arrow function/sumof arrow.js
+++
@@ -1,7 +0,0 @@
-let sum = numberArray.reduce((currentTotal, item) => {
-
-    return currentTotal + item
-
-}, 0)
-
-console.log("Sum of elements in Number Array: " + sum)
